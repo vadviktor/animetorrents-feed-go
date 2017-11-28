1eedd8a33da28d3ffd12495837377fc69b30a06b
Viktor (Icon) VAD <vad.viktor@gmail.com>
2017-11-28 11:18:36 +0000

    Upload to AWS S3.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./animetorrents-feed
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -a -o ./animetorrents-feed.linux-armv7
